2153f3d9f53175dc813dec4f8c3d1f5f52228575
Tomasz Klim <github@tomaszklim.pl>
2018-08-21 18:41:24 +0200

    fixed testing, if Azure account is configured

--- utils/test-account.sh
+++ utils/test-account.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if [ "`az account list |grep Enabled`" = "" ]; then
+if [ -f /root/.azure/accessTokens.json ] && [ "`az account list |grep Enabled`" = "" ]; then
 	echo "error: Azure account not configured, or without active subscriptions"
 fi
