abdc2869ef59f0a3d39fc251033690be4fece72c
Tomasz Klim <github@tomaszklim.pl>
2018-08-27 21:09:19 +0200

    unified create-ssh-key argument list

--- utils/create-ssh-key.sh
+++ utils/create-ssh-key.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ "$1" = "" ]; then
-	echo "usage: $0 <ssh-key-name>"
+if [ "$2" = "" ]; then
+	echo "usage: $0 <region> <ssh-key-name>"
 	exit 1
 fi
 
-name=$1
+name=$2
 key=/etc/local/.ssh/id_azure_$name
 
 if [ -f $name ] || [ -f $key ]; then
